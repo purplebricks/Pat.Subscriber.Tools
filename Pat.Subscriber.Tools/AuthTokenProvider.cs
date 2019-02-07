aca6ff38b0ecf46b898becb2cba651e37fb81c70
Rob White <rob@purplebricks.com>
2019-02-07 11:19:28 +0000

    Refactor AuthToken Handling

---
+++ Pat.Subscriber.Tools/AuthTokenProvider.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Pat.Subscriber.Tools
+{
+    public class AuthTokenProvider
+    {
+        private string _tokenValue = string.Empty;
+        private DateTime _tokenExpiresAtUtc = DateTime.MinValue;
+
+        private string _clientId;
+        private string _clientSecret;
+        private string _tenantId;
+        private readonly FileTokenCache _tokenCache;
+
+        private const string PowerShellClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
+        private const string AzureManagementResource = "https://management.azure.com/";
+
+        public AuthTokenProvider()
+        {
+            _tokenCache = new FileTokenCache();
+        }
+
+
+        public string AuthUser
+        {
+            get
+            {
+                var handler = new JwtSecurityTokenHandler();
+                var token = (JwtSecurityToken)handler.ReadToken(_tokenValue);
+                var name = token.Claims.SingleOrDefault(c => c.Type == "name");
+                var upn = token.Claims.SingleOrDefault(c => c.Type == "upn");
+                var appId = token.Claims.SingleOrDefault(c => c.Type == "appid");
+                return string.IsNullOrEmpty($"{appId.Value}") 
+                    ? $"{name.Value} <{upn.Value}>"
+                    : $"{appId.Value}";
+            }
+        }
+
+        public void SetServicePrincipal(string configCommandClientId, string configCommandClientSecret, string tenantId)
+        {
+            _clientId = configCommandClientId;
+            _clientSecret = configCommandClientSecret;
+            _tenantId = tenantId;
+        }
+
+        public async Task EnsureAuthTokenValid(HttpClient client)
+        {
+            // Check to see if the token has expired before requesting one.
+            // We will go ahead and request a new one if we are within 2 minutes of the token expiring.
+            if (_tokenExpiresAtUtc < DateTime.UtcNow.AddMinutes(-2))
+            {
+                var context = new AuthenticationContext("https://login.microsoftonline.com/common", _tokenCache);
+                string tenantFromToken = null;
+                if (context.TokenCache.Count > 0)
+                {
+                    tenantFromToken = context.TokenCache.ReadItems().First().TenantId;
+                }
+                var tenant = tenantFromToken ?? _tenantId ?? "common";
+                context = new AuthenticationContext("https://login.microsoftonline.com/" + tenant, _tokenCache);
+
+                AuthenticationResult result;
+                if (!string.IsNullOrEmpty(_clientId) && !string.IsNullOrEmpty(_clientSecret))
+                {
+                    result = await context.AcquireTokenAsync(
+                        AzureManagementResource,
+                        new ClientCredential(_clientId, _clientSecret));
+                }
+                else
+                {
+                    try
+                    {
+                        result = await context.AcquireTokenSilentAsync(
+                            AzureManagementResource,
+                            PowerShellClientId);
+                    }
+                    catch (AdalSilentTokenAcquisitionException)
+                    {
+                        var codeResult = await context.AcquireDeviceCodeAsync(
+                            AzureManagementResource,
+                            PowerShellClientId);
+                        Console.WriteLine("You need to sign in.");
+                        Console.WriteLine(codeResult.Message);
+                        result = await context.AcquireTokenByDeviceCodeAsync(codeResult);
+                    }
+                }
+
+                if (string.IsNullOrEmpty(result.AccessToken))
+                {
+                    throw new AuthenticationFailureException("Blank Auth Token");
+                }
+
+                _tokenExpiresAtUtc = result.ExpiresOn.UtcDateTime;
+                _tokenValue = result.AccessToken;
+                Console.WriteLine($"Authenticated as {AuthUser}");
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _tokenValue);
+            }
+        }
+
+        public void Logout()
+        {
+            _tokenCache.Clear();
+        }
+    }
+}
--- Pat.Subscriber.Tools/AzureHttpClient.cs
+++ Pat.Subscriber.Tools/AzureHttpClient.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,15 +13,10 @@ namespace Pat.Subscriber.Tools
 {
     public class AzureHttpClient
     {
-        private string _tokenValue = string.Empty;
-        private DateTime _tokenExpiresAtUtc = DateTime.MinValue;
-        private const string PowerShellClientId = "1950a258-227b-4e31-a9cf-717495945fc2";
-        private const string AzureManagementResource = "https://management.azure.com/";
         private readonly HttpClient _client;
-        private string _clientId;
-        private string _clientSecret;
-        private string _tenantId;
-        private readonly FileTokenCache _tokenCache;
+        private readonly AuthTokenProvider _authTokenProvider = new AuthTokenProvider();
+
+        public string AuthUser => _authTokenProvider.AuthUser;
 
         static AzureHttpClient()
         {
@@ -34,12 +27,11 @@ namespace Pat.Subscriber.Tools
         public AzureHttpClient()
         {
             _client = new HttpClient();
-            _tokenCache = new FileTokenCache();
         }
 
         public void Logout()
         {
-            _tokenCache.Clear();
+            _authTokenProvider.Logout();
         }
 
         public async Task<HttpStatusCode> GetStatusCode(Uri uri)
@@ -50,7 +42,7 @@ namespace Pat.Subscriber.Tools
 
         public async Task Put(Uri uri, object payload)
         {
-            await EnsureAuthTokenValid();
+            await _authTokenProvider.EnsureAuthTokenValid(_client);
 
             var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
             
@@ -64,7 +56,7 @@ namespace Pat.Subscriber.Tools
 
         public async Task Delete(Uri uri)
         {
-            await EnsureAuthTokenValid();
+            await _authTokenProvider.EnsureAuthTokenValid(_client);
 
             var result = await RetryHonouringRetryAfter.ExecuteAsync(() => _client.DeleteAsync(BuildManagementUri(uri)));
 
@@ -90,7 +82,7 @@ namespace Pat.Subscriber.Tools
 
         private async Task<HttpResponseMessage> Get(Uri uri)
         {
-            await EnsureAuthTokenValid();
+            await _authTokenProvider.EnsureAuthTokenValid(_client);
 
             var result = await RetryHonouringRetryAfter.ExecuteAsync(() => _client.GetAsync(BuildManagementUri(uri)));
 
@@ -134,79 +126,9 @@ namespace Pat.Subscriber.Tools
             throw new InvalidOperationException(message);
         }
 
-        public string AuthUser
-        {
-            get
-            {
-                var handler = new JwtSecurityTokenHandler();
-                var token = (JwtSecurityToken)handler.ReadToken(_tokenValue);
-                var name = token.Claims.SingleOrDefault(c => c.Type == "name");
-                var upn = token.Claims.SingleOrDefault(c => c.Type == "upn");
-                var appId = token.Claims.SingleOrDefault(c => c.Type == "appid");
-                return string.IsNullOrEmpty($"{appId.Value}") 
-                    ? $"{name.Value} <{upn.Value}>"
-                    : $"{appId.Value}";
-            }
-        }
-
-        private async Task EnsureAuthTokenValid()
-        {
-            // Check to see if the token has expired before requesting one.
-            // We will go ahead and request a new one if we are within 2 minutes of the token expiring.
-            if (_tokenExpiresAtUtc < DateTime.UtcNow.AddMinutes(-2))
-            {
-                var context = new AuthenticationContext("https://login.microsoftonline.com/common", _tokenCache);
-                string tenantFromToken = null;
-                if (context.TokenCache.Count > 0)
-                {
-                    tenantFromToken = context.TokenCache.ReadItems().First().TenantId;
-                }
-                var tenant = tenantFromToken ?? _tenantId ?? "common";
-                context = new AuthenticationContext("https://login.microsoftonline.com/" + tenant, _tokenCache);
-
-                AuthenticationResult result;
-                if (!string.IsNullOrEmpty(_clientId) && !string.IsNullOrEmpty(_clientSecret))
-                {
-                    result = await context.AcquireTokenAsync(
-                        AzureManagementResource,
-                        new ClientCredential(_clientId, _clientSecret));
-                }
-                else
-                {
-                    try
-                    {
-                        result = await context.AcquireTokenSilentAsync(
-                            AzureManagementResource,
-                            PowerShellClientId);
-                    }
-                    catch (AdalSilentTokenAcquisitionException)
-                    {
-                        var codeResult = await context.AcquireDeviceCodeAsync(
-                            AzureManagementResource,
-                            PowerShellClientId);
-                        Console.WriteLine("You need to sign in.");
-                        Console.WriteLine(codeResult.Message);
-                        result = await context.AcquireTokenByDeviceCodeAsync(codeResult);
-                    }
-                }
-
-                if (string.IsNullOrEmpty(result.AccessToken))
-                {
-                    throw new AuthenticationFailureException("Blank Auth Token");
-                }
-
-                _tokenExpiresAtUtc = result.ExpiresOn.UtcDateTime;
-                _tokenValue = result.AccessToken;
-                Console.WriteLine($"Authenticated as {AuthUser}");
-                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _tokenValue);
-            }
-        }
-
         public void SetServicePrincipal(string configCommandClientId, string configCommandClientSecret, string tenantId)
         {
-            _clientId = configCommandClientId;
-            _clientSecret = configCommandClientSecret;
-            _tenantId = tenantId;
+            _authTokenProvider.SetServicePrincipal(configCommandClientId, configCommandClientSecret, tenantId);
         }
 
         private IAsyncPolicy<HttpResponseMessage> RetryHonouringRetryAfter
